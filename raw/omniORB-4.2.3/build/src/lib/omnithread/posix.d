b72af5a81c3a595140ca816d0feb796248a9ef8c
Myles <myles.leadbeater@gmail.com>
2020-02-19 15:18:08 +1030

    initial files

--- raw/omniORB-4.2.3/build/src/lib/omnithread/posix.d
+++
@@ -1,5 +0,0 @@
-posix.o posix.d: ../../../../include/omnithread.h \
- ../../../../include/omnithread/posix.h \
- ../../../../include/omnithread/atomic.h \
- ../../../include/omniconfig.h \
- ../../../include/omniORB4/acconfig.h
--- raw/omniORB-4.2.3/src/lib/omniORB/omniORB4/distdate.hh
+++
@@ -1,3 +0,0 @@
-// distdate.hh -- Automatically generated file
-
-#define OMNIORB_DIST_DATE "Mon 10 Dec 23:41:29 GMT 2018"
